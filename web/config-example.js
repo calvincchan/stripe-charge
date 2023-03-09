c0c77cade1aca59c4b6c7a81a688db8be9f17bad
Calvin <calvin@edu.ink>
2023-03-09 13:50:54 -0800

    chore: add client-side token generation form - WIP

---
+++ web/config-example.js
@@ -0,0 +1,9 @@
+var config = {
+  COMPANY_NAME: "ABC Company",
+  LOCALE: "en_US",
+  STRIPE_KEY: "pk_test_5rJkfyyI1bq2qG8qoS6CKVnw",
+  STRIPE_ENDPOINT: "https://fnq5fc0lg9.execute-api.ap-southeast-1.amazonaws.com/charge",
+  PAYPAL_ENV: "production",
+  PAYPAL_KEY_SANDBOX: "(please enter)",
+  PAYPAL_KEY_PRODUCTION: "(please enter)",
+};
